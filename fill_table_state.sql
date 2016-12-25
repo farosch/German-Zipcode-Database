5e0f2d7b07b84fe550ff2f31c4389243490a6d14
farosch <faryan.rezagholi@gmail.com>
2016-12-25 23:38:29 +0100

    added insert statements

---
+++ fill_table_state.sql
@@ -0,0 +1,17 @@
+INSERT INTO `state` (`id`, `name`) VALUES
+(1,'Bayern'),
+(2,'Baden-Württemberg'),
+(3,'Rheinland-Pfalz'),
+(4,'Mecklenburg-Vorpommern'),
+(5,'Sachsen-Anhalt'),
+(6,'Brandenburg'),
+(7,'Niedersachsen'),
+(8,'Schleswig-Holstein'),
+(9,'Nordrhein-Westfalen'),
+(10,'Thüringen'),
+(11,'Hessen'),
+(12,'Sachsen'),
+(13,'Berlin'),
+(14,'Saarland'),
+(15,'Bremen'),
+(16,'Hamburg');
